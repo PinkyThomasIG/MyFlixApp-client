0221ba023f86ab213e555d1aa1543a9859914f5f
PinkyThomasIG <pinkymukalal@gmail.com>
2025-02-17 14:15:28 +0100

    Loginview created

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,14 @@
+export const LoginView = () => {
+  return (
+    <form>
+      <label>
+        Username:
+        <input type="text" />
+      </label>
+      <label>
+        <input type="text" />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
